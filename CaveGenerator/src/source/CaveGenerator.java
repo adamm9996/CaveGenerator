854f447893e34d9575ecf13eb0828c6d7be35c7b
adamm9996 <adamm9996@gmail.com>
2015-07-24 20:43:57 +0100

    Class CaveGenerator - Handles logic for generating pseudorandom cave. Class Run - Displays cave.

---
+++ CaveGenerator/src/source/CaveGenerator.java
@@ -0,0 +1,11 @@
+package source;
+
+public class CaveGenerator
+{
+
+	public CaveGenerator()
+	{
+		
+	}
+
+}
---
+++ CaveGenerator/src/source/Run.java
@@ -0,0 +1,9 @@
+package source;
+
+public class Run
+{
+	public static void main(String[] args)
+	{
+
+	}
+}
