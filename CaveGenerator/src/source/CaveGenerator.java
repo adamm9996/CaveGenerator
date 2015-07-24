20a0ac77a0809ec5c36b88bbaef601f2e0b33d83
adamm9996 <adamm9996@gmail.com>
2015-07-24 23:55:15 +0100

    Added smoothing algorithm to caveGenerator, making the map resemble a realistic cave rather than noise. Added iterate variable to constructor which determines how many iterations through the smoothing algorithm the generator does.

--- CaveGenerator/src/source/CaveGenerator.java
+++ CaveGenerator/src/source/CaveGenerator.java
@@ -1,11 +1,85 @@
 package source;
 
+import java.util.Random;
+
 public class CaveGenerator
 {
-
-	public CaveGenerator()
+	private int width, height;
+	private int[][] map;
+	private int fillPercent;
+	private int iterate;
+	private Random pseudoRand;
+	
+	public CaveGenerator(int width, int height, int fillPercent, int iterate)
 	{
-		
+		this.width = width;
+		this.height = height;
+		this.map = new int[width][height];
+		this.fillPercent = (fillPercent > 100 || fillPercent < 0) ? 50 : fillPercent;
+		this.pseudoRand = new Random();
+		this.iterate = iterate;
 	}
 
+	public CaveGenerator(int width, int height, int fillPercent, int iterate, String seed)
+	{
+		this.width = width;
+		this.height = height;
+		this.map = new int[width][height];
+		this.fillPercent = (fillPercent > 100 || fillPercent < 0) ? 50 : fillPercent;
+		this.pseudoRand = new Random(seed.hashCode());
+		this.iterate = iterate;
+	}
+	
+	public int[][] generate()
+	{
+		randomFillMap();
+		for (int t = 0; t < iterate; t++)
+			smoothMap();
+		
+		return map;
+	}
+
+	
+	private void smoothMap()
+	{
+		for (int w = 0; w < width; w++)
+			for (int h = 0; h < height; h++)
+			{
+				int surroundingFilledTiles = getSurroundingCount(w,h);
+				if (surroundingFilledTiles > 4)
+					map[w][h] = 1;
+				else if (surroundingFilledTiles < 4)
+					map[w][h] = 0;
+			}
+	}
+	
+	int getSurroundingCount(int x, int y)
+	{
+		int wallCount = 0;
+		for (int neighbourX = x-1; neighbourX <= x+1; neighbourX++)
+			for (int neighbourY = y-1; neighbourY <= y+1; neighbourY++)
+				if (neighbourX >= 0 && neighbourX < width && neighbourY >= 0 && neighbourY < height)
+				{
+					if (neighbourX != x || neighbourY != y)
+						wallCount += map[neighbourX][neighbourY];
+				}
+				else 
+				{
+					wallCount++;
+				}
+				
+		return wallCount;
+	}
+	
+	private void randomFillMap()
+	{
+		//initially fills the map with 1s and 0s that look like noise. Ratio depends on wallPercentage
+		for (int w = 0; w < width; w++)
+			for (int h = 0; h < height; h++)
+				if (w == 0 || w == width-1 || h == 0 || h == height-1)//ensure the borders are always filled
+					map[w][h] = 1;
+				else
+					map[w][h] = (pseudoRand.nextInt(100) < fillPercent) ? 1 : 0;
+	}
+		
 }
--- CaveGenerator/src/source/Run.java
+++ CaveGenerator/src/source/Run.java
@@ -1,9 +1,50 @@
 package source;
 
-public class Run
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Run extends JPanel
 {
+	static JFrame frame = new JFrame();
+	static Run run = new Run();
+	int[][] map;
+	
+	final static int blockSize = 5;		//size of each cell
+	final static int width = 200;		//width of map in cells
+	final static int height = 100;		//height of map in cells
+	final static int fillPercent = 101;	//percentage of map filled in
+	final static int iterate = 10;		//number of iterations of the smoothing algorithm the generator does
+	final static boolean seeded = true;	//true: given string, false: random string
+	final static String seed = "cat";	//seed used to generate the map, if any
+	
+	
 	public static void main(String[] args)
 	{
-
+		CaveGenerator caveGenerator;
+		if (seeded)
+			caveGenerator = new CaveGenerator(width, height, fillPercent, iterate, seed);
+		else
+			caveGenerator = new CaveGenerator(width, height, fillPercent, iterate);
+		
+		run.map = caveGenerator.generate();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(width*blockSize, height*blockSize);
+		frame.add(run);
+		frame.setVisible(true);
+	}
+	
+	public void paint(Graphics g)
+	{
+		super.paint(g);
+		
+		for (int w = 0; w < width; w++)
+			for (int h = 0; h < height; h++)
+			{
+				g.setColor((map[w][h] == 1) ? Color.BLACK : Color.WHITE);
+				g.fillRect(blockSize*w, blockSize*h, blockSize, blockSize);
+			}
 	}
 }
